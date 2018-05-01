f9086b13920f68793c753769ac6dd1d2b5efd5fe
david <tlan8029@uni.usyd.edu.au>
2018-05-01 17:37:58 +1000

    update file -- list the files when users type a directory.

--- my-project/chapter5/index.js
+++ my-project/chapter5/index.js
@@ -9,25 +9,40 @@ fs.readdir(process.cwd(), function(err, files){
       }
     }
   }
+
   process.stdin.setEncoding('utf8');
   process.stdin.on('data', (data) => {
       const chunk = data;
       if(chunk !== null) {
         let path = `${process.cwd()}/${chunk.trim()}`;
         fs.stat(path, function(err, stats){
-            if(!err && stats.isFile()) {
+            if(!err) {
+              if(stats.isFile()) { ///file
                 //console.log(stats);
                 process.stdin.pause();
                 //show info about the file
                 fs.readFile(path, {encoding:'utf8', flag:'r'}, (err, data) => {
                   if(err) throw err;
-                  console.log(data);
+                    console.log(data);
                 });
+              }else if(stats.isDirectory()){  ///directory -- list files under this directory
+                  fs.readdir(path, (err, files) => {
+                    files.forEach(function(file){
+                      console.log(file);
+                    })
+                    process.stdin.pause();
+                  });
+              }else{
+                //
+              }
+            }else if(err.code === "ENOENT"){
+                console.log("select a file");
             }
         });
       }
-      console.log("select a file");
   });
+  console.log("select a file");
+
 
   // process.stdin.on('readable', () => {
   //     const chunk = process.stdin.read();
